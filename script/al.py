a24a8333343bbf36d525e5b80f4f054380df61eb
schimar <schimar@gmail.com>
2021-03-05 10:38:54 +0100

    pwDist matrices

--- script/al.py
+++ script/al.py
@@ -35,9 +35,11 @@ variants = zarr.open_group(zarrPath, mode='r')
 #NOTE: ids will already be read in common.smk, so no need to do this here (for now, we need to, though)
 ids = pd.read_table('samples109.txt', sep='\t', index_col=False)
 ids['id_nest'] = ids['id'] + '_' + ids['nest']
+ids = ids.sort_values(by='nest')
+
 
 nInds = ids.groupby(by= ['nest', 'pop']).count()['sample']
-np.all(list(variants['samples']) == ids['id'].values)
+np.all(list(variants['samples']) == ids['id_nest'].values)
 
 samples = list(variants['samples'])
 subsIndex = [samples.index(s) for s in ids['id_nest']]
@@ -82,17 +84,10 @@ gtseg_is_het = pd.DataFrame(gtseg_vars.is_het()).describe().transpose()
 gtseg_is_het.index = ids['id_nest']
 gtseg_is_het['het'] = gtseg_is_het['freq']/gtseg_is_het['count']
 gtseg_is_het['hom'] = 1-gtseg_is_het['het']
+gtseg_is_het.to_csv(os.path.join(statsPath, 'nSegHets.txt'), header=True, index=True, sep= '\t')
 
 
 
-
-
-
-
-
-
-
-### nests stats
 ac_nests_vars = gtvars.count_alleles_subpops(nests, max_allele=1)
 
 # nSeg per pop/nest as DataFrame:
@@ -106,6 +101,27 @@ nestseg = dict()
 for nest in ac_nests_vars.keys():
     nestseg[nest] = ac_nests_vars[nest].count_segregating()
 pd.Series(nestseg, index=nestseg.keys()).to_csv(os.path.join(statsPath, 'nSegAlleles.nest.txt'), header= False, index=True, sep= '\t')
+
+
+
+
+# pairwise distance matrix
+
+dvar = al.pairwise_distance(gtvars.to_n_alt(), metric= 'cityblock')
+
+
+# heatmap with dendrogram
+
+condensedDvar = scipy.spatial.distance.squareform(dvar)
+
+n2col = dict(zip(ids['nest'].unique(), sns.color_palette()))
+rowCols = np.array(ids['nest'].map(n2col))
+
+cDdf = pd.DataFrame(condensedDvar, index= ids['nest'], columns=ids['id'])
+g = sns.clustermap(cDdf, row_colors= rowCols, cmap= 'jet')
+g.ax_heatmap.set_xticklabels(g.ax_heatmap.get_xmajorticklabels(), fontsize = 12)     #ha= 'right', rotation= 40
+g.ax_heatmap.set_yticklabels(g.ax_heatmap.get_ymajorticklabels(), fontsize = 12)
+
 
 
 
