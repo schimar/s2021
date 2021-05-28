256d03ad55d5fc0a574a3099ca0132e40224176d
schimar <schimar@gmail.com>
2021-05-28 09:53:58 +0200

    check if folder exists and create if not

--- script/bslmm_hyp.r
+++ script/bslmm_hyp.r
@@ -14,8 +14,18 @@ hyp <- read.table(infile, header=T)
 # from http://romainvilloutreix.alwaysdata.net/romainvilloutreix/wp-content/uploads/2017/01/gwas_gemma-2017-01-17.pdf
 
 #path <- dirname(infile)
+mainDir <- dirname(infile)
+subDir <- unlist(strsplit(basename(infile), '.', fixed= T))[1]
+#substr(basename(infile), 1, nchar(basename(infile))-4)
+outDir <- file.path(mainDir, subDir)
 
-outfile <- paste0(substr(infile, 1, nchar(infile)-3), 'pdf')
+if (!dir.exists(outDir)){
+	dir.create(outDir)
+} else {
+    print(paste0(outDir, " already exists"))
+}
+
+outfile <- paste0(file.path(outDir, subDir), '.hyp.pdf')
 
 # plot traces and distributions of hyperparameters
 # ==============================================================================
